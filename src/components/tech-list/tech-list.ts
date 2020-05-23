dfced3e4874d356f012a099ca0c4eab42a6c5389
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-05-23 19:06:21 +0200

    refacto

--- src/components/tech-list/tech-list.ts
+++ src/components/tech-list/tech-list.ts
@@ -27,22 +27,9 @@ export default class TechList extends BaseComponent
             EBuilder(e.currentTarget).swap(this.select('tech-item'), true)
         }
 
-        this.select('ul')!.append(...this.props.stack.map((name) => {
-            return EBuilder('li').set({
-                children: [
-                    EBuilder(new TechItem(name)).set({
-                        listeners: ['click', swapIt]
-                    })
-                ],
-            }).element
-        }))
-
-        EBuilder(this.select('ul')).setChildren(() => {
-            return this.props.stack.map((name) => {
-                return EBuilder('li').setChildren(
-                    EBuilder(new TechItem(name)).setListeners(['click', swapIt])
-                )
-            })
-        })
+        this.props.stack.forEach((name) => EBuilder(new TechItem(name))
+            .setListeners(['click', swapIt])
+            .into(EBuilder('li').into(this.select('ul')))
+        )
     }
 }
